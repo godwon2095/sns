a3ec9a249d408f2a187d66e6e3f23f0017445b92
sungwon <jsw2095@naver.com>
2019-08-27 23:51:27 +0900

    git ignore add

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 from share.timestamp import TimeStampedModel
+from django.utils.translation import ugettext_lazy as _
 
 
 class Post(TimeStampedModel):
@@ -9,13 +10,21 @@ class Post(TimeStampedModel):
     content = models.TextField()
     view_count = models.IntegerField(default=0)
     image = models.ImageField(upload_to="img/")
-    likes = models.ManyToManyField(User, related_name="liked_users")
+    like_users = models.ManyToManyField(User,
+                                        blank=True,
+                                        related_name='like_users',
+                                        through='Like')
 
     def __str__(self):
        return self.title
 
+    @property
     def comments(self):
         return Comment.objects.filter(post=self)
+
+    @property
+    def likes_count(self):
+        return self.like_users.count()
 
 
 class Comment(TimeStampedModel):
@@ -29,3 +38,15 @@ class Comment(TimeStampedModel):
 
     def __str__(self):
         return self.message
+
+
+class Like(TimeStampedModel):
+    user = models.ForeignKey(User, verbose_name=_('작성자'), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, verbose_name=_('게시글'), on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '게시글 좋아요'
+        verbose_name_plural = '게시글 좋아요'
+        unique_together = (
+            ('user', 'post')
+        )
